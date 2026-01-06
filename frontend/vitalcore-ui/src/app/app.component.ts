0bee6a941609c18ea6d81694cc3214555eb12b23
Alicia Cortes <alis04cortesg@gmail.com>
2026-01-05 23:35:02 -0600

    Implementar header y footer

--- frontend/vitalcore-ui/src/app/app.component.ts
+++ frontend/vitalcore-ui/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/layout/header/header.component';
+import { FooterComponent } from './core/layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'vitalcore-ui';
-}
+
+export class AppComponent {}
--- frontend/vitalcore-ui/src/app/core/layout/header/header.component.ts
+++ frontend/vitalcore-ui/src/app/core/layout/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
 
-}
+export class HeaderComponent {}
